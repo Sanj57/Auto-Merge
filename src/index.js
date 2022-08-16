fa76e23b3731ca598938472d85574308ebdf8d8e
Sanjali Nagare <sanjalinagare@Sanjalis-MacBook-Pro.local>
2022-08-16 16:15:27 -0700

    Auto Merge

---
+++ src/index.js
@@ -0,0 +1,29 @@
+'use strict'
+const core = require('@actions/core')
+const { GitHub, context } = require('@actions/github')
+
+const main = async () => {
+  const token = core.getInput('github-token')
+  const number = core.getInput('number')
+  const method = core.getInput('method')
+  const repoString = core.getInput('repo')
+  console.log('its working');
+
+  let repoObject
+  if (repoString) {
+    const [owner, repo] = repoString.split('/')
+    repoObject = { owner, repo }
+  } else {
+    repoObject = context.repo
+  }
+
+  const octokit = new GitHub(token)
+
+  await octokit.pulls.merge({
+    ...repoObject,
+    pull_number: number,
+    merge_method: method
+  })
+}
+
+main().catch(err => core.setFailed(err.message))
